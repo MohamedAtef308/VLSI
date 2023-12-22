b576af190d853b4e4f55d1f52625f77e74005811
Sofa <sofa5060@gmail.com>
2023-12-22 23:22:30 +0200

    optimized bit wise adder

--- Multipliers/Tree Multiplier/BWAdder.v
+++ Multipliers/Tree Multiplier/BWAdder.v
@@ -5,13 +5,6 @@ module BWAdder (
   output wire [63:0] result,
   output wire [63:0] carry
 );
-  assign result[63] = a[63] ^ b[63] ^ c[63];
-  assign carry[0] = 1'b0;
-  genvar i;
-  generate
-    for (i = 0; i < 63; i = i + 1) begin: bitwiseAdd
-      assign result[i] = a[i] ^ b[i] ^ c[i];
-      assign carry[i + 1] = (a[i] & b[i]) | (c[i] & (a[i] ^ b[i]));
-    end
-  endgenerate
+  assign result = a ^ b ^ c;
+  assign carry = ((a & b) | (c & (a ^ b))) << 1;
 endmodule
